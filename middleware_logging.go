1b6e2dee469296a1cde7ddda414540f2459b20aa
John Allison <jrallison@gmail.com>
2013-07-20 09:49:29 -0500

    Update logging

--- middleware_logging.go
+++ middleware_logging.go
@@ -2,12 +2,26 @@ package workers
 
 import (
 	"fmt"
+	"time"
 )
 
 type MiddlewareLogging struct{}
 
 func (l *MiddlewareLogging) Call(queue string, message *Msg, next func()) {
-	fmt.Println("Before")
+	prefix := fmt.Sprint(queue, " JID-", message.Jid())
+
+	start := time.Now()
+	fmt.Println(prefix, "start")
+	fmt.Println(prefix, "args: ", message.Args().ToJson())
+
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println(prefix, "fail:", time.Since(start))
+			panic(e)
+		}
+	}()
+
 	next()
-	fmt.Println("After")
+
+	fmt.Println(prefix, "done:", time.Since(start))
 }
