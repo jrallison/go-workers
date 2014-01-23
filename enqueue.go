f4bf97a05605a101e6b04c458c52da97c5ad6dab
Keith Brisson <kbrisson@gmail.com>
2014-01-23 12:58:57 -0500

    Add 'jid' and 'enqueued_at' and optional options

--- enqueue.go
+++ enqueue.go
@@ -1,24 +1,58 @@
 package workers
 
 import (
+	"crypto/rand"
 	"encoding/json"
+	"fmt"
+	"io"
+	"time"
 )
 
 type EnqueueData struct {
-	Class string `json:"class"`
-	Args  interface{} `json:"args"`
+	Class      string      `json:"class"`
+	Args       interface{} `json:"args"`
+	Jid        string      `json:"jid"`
+	EnqueuedAt float64     `json:"enqueued_at"`
+	EnqueueOptions
+}
+
+type EnqueueOptions struct {
+	RetryCount int     `json:"retry_count,omitempty"`
+	Retry      bool    `json:"retry,omitempty"`
+	At         float64 `json:"at,omitempty"`
+}
+
+func generateJid() string {
+	// Return 12 random bytes as 24 character hex
+	b := make([]byte, 12)
+	_, err := io.ReadFull(rand.Reader, b)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%x", b)
 }
 
 func Enqueue(queue, class string, args interface{}) error {
+	return EnqueueWithOptions(queue, class, args, EnqueueOptions{})
+}
+
+func EnqueueWithOptions(queue, class string, args interface{}, opts EnqueueOptions) error {
 	conn := Config.Pool.Get()
 	defer conn.Close()
 
-	bytes, err := json.Marshal(EnqueueData{class, args})
+	data := EnqueueData{
+		Class:          class,
+		Args:           args,
+		Jid:            generateJid(),
+		EnqueuedAt:     float64(time.Now().UnixNano()) / 1000000000,
+		EnqueueOptions: opts,
+	}
+	bytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
-	_, err = conn.Do("sadd", Config.namespace + "queues", queue)
+	_, err = conn.Do("sadd", Config.namespace+"queues", queue)
 	if err != nil {
 		return err
 	}
