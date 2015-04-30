f4650d9749ada0088a4c5864bebfc0e81bc97aa3
yujun <jyu@firewinggames.com>
2015-04-30 16:21:42 +0800

    try to use gopkg.in/redis.v2 replace old one

--- config.go
+++ config.go
@@ -1,85 +1,75 @@
 package workers
 
 import (
-	"strconv"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
+	"github.com/fatih/structs"
+	"gopkg.in/redis.v2"
 )
 
 type config struct {
 	processId    string
 	Namespace    string
-	PollInterval int
-	Pool         *redis.Pool
+	PollInterval uint16
+	RedisClient  *redis.Client
 	Fetch        func(queue string) Fetcher
 }
 
 var Config *config
 
-func Configure(options map[string]string) {
-	var poolSize int
-	var namespace string
-	var pollInterval int
+type Options struct {
+	Process      string
+	Namespace    string
+	PollInterval uint16
+	RedisOptions *redis.Options
+}
 
-	if options["server"] == "" {
-		panic("Configure requires a 'server' option, which identifies a Redis instance")
-	}
-	if options["process"] == "" {
+func Configure(options *Options) {
+	if options.Process == "" {
 		panic("Configure requires a 'process' option, which uniquely identifies this instance")
 	}
-	if options["pool"] == "" {
-		options["pool"] = "1"
-	}
-	if options["namespace"] != "" {
-		namespace = options["namespace"] + ":"
-	}
-	if options["poll_interval"] == "" {
-		pollInterval = 15
-	} else {
-		pollIntervalInt, err := strconv.Atoi(options["poll_interval"])
-		if err != nil {
-			pollInterval = 15
-		} else {
-			pollInterval = pollIntervalInt
-		}
+
+	ops := &Options{
+		Namespace:    "",
+		PollInterval: 500,
 	}
 
-	poolSize, _ = strconv.Atoi(options["pool"])
+	MergeSimpleStruct(ops, options)
+
+	redisOpts := &redis.Options{
+		Network:      "tcp",
+		Addr:         "localhost:6379",
+		Password:     "",
+		DB:           0,
+		DialTimeout:  1 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		PoolSize:     20,
+		IdleTimeout:  60 * time.Second}
+
+	MergeSimpleStruct(redisOpts, options.RedisOptions)
+
+	client := redis.NewClient(redisOpts)
 
 	Config = &config{
-		options["process"],
-		namespace,
-		pollInterval,
-		&redis.Pool{
-			MaxIdle:     poolSize,
-			IdleTimeout: 240 * time.Second,
-			Dial: func() (redis.Conn, error) {
-				c, err := redis.Dial("tcp", options["server"])
-				if err != nil {
-					return nil, err
-				}
-				if options["password"] != "" {
-					if _, err := c.Do("AUTH", options["password"]); err != nil {
-						c.Close()
-						return nil, err
-					}
-				}
-				if options["database"] != "" {
-					if _, err := c.Do("SELECT", options["database"]); err != nil {
-						c.Close()
-						return nil, err
-					}
-				}
-				return c, err
-			},
-			TestOnBorrow: func(c redis.Conn, t time.Time) error {
-				_, err := c.Do("PING")
-				return err
-			},
-		},
+		ops.Process,
+		ops.Namespace,
+		ops.PollInterval,
+		client,
 		func(queue string) Fetcher {
 			return NewFetch(queue, make(chan *Msg), make(chan bool))
 		},
+	}
+}
+
+func MergeSimpleStruct(dst interface{}, src interface{}) {
+	srcM := structs.Map(src)
+	srcS := structs.New(src)
+	dstS := structs.New(dst)
+
+	for name, value := range srcM {
+		if !srcS.Field(name).IsZero() {
+			dstS.Field(name).Set(value)
+		}
 	}
 }
